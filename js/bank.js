6947055d744314f9fe196336799c5e67992bcd31
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-02-12 01:40:56 +0600

    k

--- js/bank.js
+++ js/bank.js
@@ -9,9 +9,16 @@ document.getElementById('deposit').addEventListener("click", function () {
     const totalAmount = document.getElementById('total');
     const totalAmmountFinal = totalAmount.innerText;
 
-    deposiTotal.innerText = parseFloat(depositInput) + parseFloat(depositFinal);
-    totalAmount.innerText = parseFloat(depositInput) + parseFloat(totalAmmountFinal);
+
     userDepositInput.value = '';
+    if (depositInput > 0) {
+        deposiTotal.innerText = parseFloat(depositInput) + parseFloat(depositFinal);
+        totalAmount.innerText = parseFloat(depositInput) + parseFloat(totalAmmountFinal);
+        alert("SuccessFully Added On Your Account")
+    }
+    else {
+        alert("Please Deposite Ammount")
+    }
 
 
 });
@@ -30,7 +37,7 @@ document.getElementById('Withdrow').addEventListener("click", function () {
 
 
     userWithderowInput.value = '';
-    if (parseFloat(totalAmmountFinal) > parseFloat(withdrowInput)) {
+    if (parseFloat(totalAmmountFinal) >= parseFloat(withdrowInput) && withdrowInput > 0) {
         withdrowTotal.innerText = parseFloat(withdrowInput) + parseFloat(withdrowAmount);
         totalAmmount.innerText = parseFloat(totalAmmountFinal) - parseFloat(withdrowInput);
         alert("Congratulation! You can withdrow ", withdrowTotal.innerText = parseFloat(withdrowInput) + parseFloat(withdrowAmount), " Doller from Your Account")
